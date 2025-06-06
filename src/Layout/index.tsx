f7001b29b4001a839c0a32e10737c2344911bf38
Vardy41 <kokoko1325@gmail.com>
2025-06-06 02:00:57 +0200

    usuniecie kilku plikow, przekonwertowanie tla na webp i ustawienie nowego tla

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,14 +1,17 @@
-import { Background } from "../components/ui/Background";
 import { NavBar } from "../components/ui/NavBar";
 import { About } from "../components/sections/About";
 import { Home } from "../components/sections/Home";
 import { Skills } from "../components/sections/Skills";
 import { Projects } from "../components/sections/Projects";
+import bg from "../assets/svg/backgrounds/mainBg.webp";
 
 export const Layout = () => {
 	return (
 		<>
-			<Background />
+			<div
+				className="fixed inset-0 -z-10 bg-slate-900 bg-cover bg-center bg-no-repeat"
+				style={{ backgroundImage: `url(${bg})` }}
+			/>
 			<NavBar />
 			<main className="min-h-screen relative px-4 sm:px-6 lg:px-8">
 				<Home />
--- src/components/ui/GradientBlob/index.tsx
+++
@@ -1,5 +0,0 @@
-import type { GradientBlobProps } from "../../../types/types";
-
-export const GradientBlob = ({ className, color }: GradientBlobProps) => (
-	<div className={`${className} ${color} rounded-full filter blur-3xl`} />
-);
