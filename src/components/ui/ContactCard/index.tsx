5200030a99c05e5384c1a7cc09caa139edd9ec28
Vardy41 <kokoko1325@gmail.com>
2025-06-18 18:17:25 +0200

    poprawa contact

--- src/components/ui/ContactCard/index.tsx
+++ src/components/ui/ContactCard/index.tsx
@@ -10,15 +10,18 @@ export const ContactCard = ({
 	label,
 }: ContactCardProps) => {
 	return (
-		<a href={href} key={label} target="_blank">
-			<li className="rounded-xl">
-				<CardWrapper className="flex flex-col items-center mb-2 ">
+		<li className="rounded-xl" key={label}>
+			<a
+				href={href}
+				target="_blank"
+				rel="noopener noreferrer"
+				className="block">
+				<CardWrapper className="flex flex-col items-center mb-2">
 					<Icon className="text-5xl text-white mb-1" />
-
 					<h3 className="text-white text-2xl font-bold">{socialName}</h3>
 					<Paragraph textColor="text-slate-500" content={nick} />
 				</CardWrapper>
-			</li>
-		</a>
+			</a>
+		</li>
 	);
 };
