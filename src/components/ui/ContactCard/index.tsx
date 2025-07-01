ecb4b19b0c429a0a07210f38b3ccfa585a3315db
Vardy41 <kokoko1325@gmail.com>
2025-07-01 18:18:30 +0200

    dodanie sr-only

--- src/components/ui/ContactCard/index.tsx
+++ src/components/ui/ContactCard/index.tsx
@@ -13,16 +13,12 @@ export const ContactCard = ({
 }: ContactCardWithWrapperProps) => {
 	const content = (
 		<li className="rounded-xl" key={label}>
-			<a
-				aria-label={`Ikona ${socialName}`}
-				href={href}
-				target="_blank"
-				rel="noopener noreferrer"
-				className="block">
+			<a href={href} target="_blank" rel="noreferrer" className="block">
 				<CardWrapper className="flex flex-col items-center mb-2">
 					<Icon className="text-5xl text-white mb-1" />
 					<h3 className="text-white text-2xl font-bold">{socialName}</h3>
 					<Paragraph textColor="text-slate-500" content={nick} />
+					<span className="sr-only">{`Otwórz profil ${socialName} w nowej karcie`}</span>
 				</CardWrapper>
 			</a>
 		</li>
