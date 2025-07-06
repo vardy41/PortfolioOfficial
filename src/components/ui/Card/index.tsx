747b02aafcf0aa11cd7794b4d1e10ee0e8967c7e
Vardy41 <kokoko1325@gmail.com>
2025-07-06 10:47:20 +0200

    Card zmiana wysokosci

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { CardProps } from "../../../types/types";
 export const Card = React.memo(({ title, description, company }: CardProps) => (
-	<div className="card p-4 rounded-xl bg-slate-800 shadow-md h-[7rem]">
+	<div className="card p-4 rounded-xl bg-slate-800 shadow-md h-[8rem]">
 		<h3 className="text-xl font-semibold mb-2 text-blue-500">{title}</h3>
 		<p className="text-slate-300 text-sm sm:text-base">{description}</p>
 		<p className="text-slate-400 text-xs sm:text-sm">{company}</p>
