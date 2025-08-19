e7761889d949ddbc6ebfa4a6db714d5dbe53c2f6
Vardy41 <kokoko1325@gmail.com>
2025-08-19 17:23:24 +0200

    index

--- src/components/ui/SkillsCard/index.tsx
+++ src/components/ui/SkillsCard/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import type { SkillsCardProps } from "../../../types/types";
 
-
 export const SkillsCard = React.memo(
 	({ icon: Icon, title, tech, textColor, bgColor }: SkillsCardProps) => {
 		return (
